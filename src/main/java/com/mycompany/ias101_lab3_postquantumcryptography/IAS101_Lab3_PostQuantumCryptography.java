7f515cac4dcbcb7442968c789d2cc743eb08f3a8
Mark John <markjohnjopia1@gmail.com>
2025-03-24 01:25:54 +0800

    initial commit

---
+++ src/main/java/com/mycompany/ias101_lab3_postquantumcryptography/IAS101_Lab3_PostQuantumCryptography.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ias101_lab3_postquantumcryptography;
+
+/**
+ *
+ * @author Mark John Jopia
+ */
+public class IAS101_Lab3_PostQuantumCryptography {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
